57278b3bc87a4f2391b8edc5e965c04b920af073
duyubo <2669561343@qq.com>
2020-12-20 00:28:31 -0600

    update lab3 source file

--- lab3/sources_1/new/Decoder2_4.v
+++ lab3/sources_1/new/Decoder2_4.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 12/18/2020 12:02:53 AM
+// Create Date: 12/18/2020 02:45:32 PM
 // Design Name: 
-// Module Name: LEDCalculator
+// Module Name: Decoder2_4
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,11 +20,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module LEDCalculator(input [3:0] a, input [3:0] b, input [1:0] s, output [6:0] D, output o, output [3:0] AN);
-    wire [4:0] s1;
-    FourBitAdder g1 ( .a(a), .b(b), .s(s1));
-    assign o = s1[4];
-    Decoder g2 (.s(s1[3:0]),.D(D));
-    Decoder2_4(.a(s), .o1(AN));
+module Decoder2_4(input [1:0] a, output [3:0] o1);
+    
+    wire [1:0]n;
+    not gNot1 (n[0], a[0]);
+    not gNot2 (n[1], a[1]);
+    
+    or g1 (o1[3],n[0],n[1]);
+    or g2 (o1[2],a[0],n[1]);
+    or g3 (o1[1],n[0],a[1]);
+    or g4 (o1[0],a[0],a[1]);
 
 endmodule
